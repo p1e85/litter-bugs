595e3218d43e1333f3ead6d24ff59adc76cacb44
patrick d. <p1e85@users.noreply.github.com>
2025-09-25 20:43:12 -0500

    Update maptest.js

--- js/maptest.js
+++ js/maptest.js
@@ -275,7 +275,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     
     authActionBtn.addEventListener('click', async () => {
-        if (isSignUpMode) await updateDoc();
+        if (isSignUpMode) await handleSignUp();
         else await handleLogIn();
     });
     document.getElementById('logoutBtn').addEventListener('click', async () => await signOut(auth));
