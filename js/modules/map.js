f6ff87973bf123dd469c58a86e4e70514dcbce80
patrick d. <p1e85@users.noreply.github.com>
2025-09-27 22:48:52 -0500

    Create map.js

---
+++ js/modules/map.js
@@ -0,0 +1,13 @@
+// This module contains all logic directly related to the Mapbox GL JS library.
+import * as state from './state.js';
+import { createPinPopup } from './ui.js';
+// ... (other necessary imports)
+
+export function initializeMap(container) {
+    // ... (logic to create new mapboxgl.Map)
+}
+
+export function initializeMapLayers() {
+    // ...
+}
+// ... (all other map-related functions: changeMapStyle, toggleMarkerVisibility, findMe, etc.)
