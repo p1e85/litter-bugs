b54a3fc04fd368c2e9494f345559565debb0da77
patrick d. <p1e85@users.noreply.github.com>
2025-09-29 17:09:35 -0500

    Update tracking.js

--- js/modules/tracking.js
+++ js/modules/tracking.js
@@ -3,12 +3,14 @@ import { state } from './config.js';
 import { createAndAddMarker, updateUserPinsSource } from './map.js';
 import { calculateRouteDistance } from './utils.js';
 import { clearCurrentSession } from './data.js';
-import { imageCompression } from '../framework/browser-image-compression.js';
+//import { imageCompression } from '../framework/browser-image-compression.js';
 
 // NOTE: This imports the image-compression library from a CDN.
 // For a production app, you might want to host this file yourself.
 // This is a stable link from the unpkg CDN
 //import imageCompression from  'https://cdn.jsdelivr.net/npm/browser-image-compression@2.0.2/dist/browser-image-compression.esm.js';
+import imageCompression from 'https://cdn.jsdelivr.net/npm/browser-image-compression@latest/dist/browser-image-compression.js';
+
 /**
  * Finds the user's current location and places a one-time marker on the map.
  */
